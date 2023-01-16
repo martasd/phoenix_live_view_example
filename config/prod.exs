0a5407dcb617a07f347106cc7915b5d84a73632e
Martin Dluhoš <dluhos.martin@gmail.com>
2023-01-16 16:14:43 +0100

    feat: generate configs for Heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,11 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :demo, DemoWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "liveview-demo.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
--- config/runtime.exs
+++ config/runtime.exs
@@ -15,30 +15,29 @@ if config_env() == :prod do
       """
 
   config :demo, Demo.Repo,
-    ssl: false,
-    socket_options: [:inet6],
+    ssl: true,
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "30")
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+  # secret_key_base =
+  #   System.get_env("SECRET_KEY_BASE") ||
+  #     raise """
+  #     environment variable SECRET_KEY_BASE is missing.
+  #     You can generate one by calling: mix phx.gen.secret
+  #     """
 
-  config :demo, DemoWeb.Endpoint,
-    server: true,
-    url: [host: "liveview-demo.fly.dev", port: 80],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
-    ],
-    secret_key_base: secret_key_base
+  # config :demo, DemoWeb.Endpoint,
+  #   server: true,
+  #   url: [host: "liveview-demo.fly.dev", port: 80],
+  #   http: [
+  #     # Enable IPv6 and bind on all interfaces.
+  #     # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+  #     # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+  #     # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+  #     ip: {0, 0, 0, 0, 0, 0, 0, 0},
+  #     port: String.to_integer(System.get_env("PORT") || "4000")
+  #   ],
+  #   secret_key_base: secret_key_base
 
   # ## Using releases
   #
